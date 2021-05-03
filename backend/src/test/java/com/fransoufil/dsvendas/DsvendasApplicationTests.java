540a3ef66b6aaea302f2c940a0c9b22e9597c683
fransoufil <francixco.sf@gmail.com>
2021-05-03 15:16:48 -0300

    Projetos criados

---
+++ backend/src/test/java/com/fransoufil/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fransoufil.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
